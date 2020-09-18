c9b5c4e7441be09a43bc9fb1c01814dc1c823d9f
Charles Knight <Charles-Knight@users.noreply.github.com>
2020-09-17 21:10:49 -0700

    Implemented BubbleSort

--- Python/SortingAlgorithms.py
+++ Python/SortingAlgorithms.py
@@ -1,5 +1,16 @@
 def BubbleSort(nums):
-    pass
+    def swap(l, idx1, idx2):
+        l[idx1] = l[idx1] + l[idx2]
+        l[idx2] = l[idx1] - l[idx2]
+        l[idx1] = l[idx1] - l[idx2]
+    
+    swaps = -1
+    while swaps != 0:
+        swaps = 0
+        for i in range(0, len(nums)-1):
+            if nums[i] > nums[i+1]:
+                swap(nums, i, i+1)
+                swaps += 1     
 
 def SelectionSort(nums):
     for position in range(0, len(nums)):
