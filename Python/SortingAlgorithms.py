db6a41997dee1e6761f87bcbda3e9b00b9bf9080
Charles Knight <Charles-Knight@users.noreply.github.com>
2020-09-17 22:03:37 -0700

    Impleneted Insertion Sort

--- Python/SortingAlgorithms.py
+++ Python/SortingAlgorithms.py
@@ -22,8 +22,19 @@ def SelectionSort(nums):
         del nums[index_of_minimum + 1]
 
 def InsertionSort(nums):
-    pass
+    def insert(input_value, l, start, end):
+        # Find appropriate position
+        insert_position = 0
+        while input_value > l[insert_position] and insert_position < end:
+            insert_position += 1
+        l.insert(insert_position, input_value)
+   
+    for index in range(1, len(nums)):
+        to_insert = nums[index]
+        del nums[index]
+        insert(to_insert, nums, 0, index)
 
+        
 def MergeSort(nums):
     pass
 
